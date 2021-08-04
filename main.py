0ac0a96a8942afbf38086193029b9f649b6adf95
martin <martin.sachin@gmail.com>
2021-08-04 17:08:43 +0200

    Added simulation runner.

--- main.py
+++ main.py
@@ -1,14 +1,116 @@
-import subprocess
+import math
+from pyepoch import picmi
+from scipy.constants import c, m_e, epsilon_0, e
 
+
+def critical(omega):
+    return omega ** 2 * m_e * epsilon_0 / e ** 2
+
+
+def get_simulation():
+    micron = 1e-6
+    femto = 1e-15
+    xmin = -10 * micron
+    xmax = -xmin
+    ymin = xmin
+    ymax = xmax
+    max_time = 50 * femto
+    grid = picmi.Cartesian2DGrid(
+        nx=249,
+        ny=249,
+        xmin=xmin,
+        xmax=xmax,
+        ymin=ymin,
+        ymax=ymax,
+        bc_xmin="simple_laser",  # simple_laser is not standard
+        bc_xmax="simple_outflow",  # simple_outflow is not standard
+        bc_ymin="periodic",
+        bc_ymax="periodic",
+        epoch_enable_bcs=True
+    )
+    solver = picmi.ElectromagneticSolver(grid)
+    simulation = picmi.Simulation(solver, max_time=max_time)
+
+    lamb = 1.06 * micron
+    omega = 2 * math.pi * c / lamb
+    den_cone = 4 * critical(omega)
+    th = 1 * micron / 2
+    ri = "abs(x - 5*{}) - sqrt(2.0) * th".format(micron)
+    ro = "abs(x - 5*{}) - sqrt(2.0) * th".format(micron)
+    xi = "3*{} - th".format(micron)
+    xo = "3*{} + th".format(micron)
+    r = "sqrt(y^2)"
+    initial_density = "where(({r} > {ri}) & ({r} < {ro}), {den_cone}, 0.0".format(
+        r=r,
+        ri=ri,
+        ro=ro,
+        den_cone=den_cone
+    )
+    initial_density = "where((x > {xi}) & (x < {xo}) & ({r} < {ri}), {den_cone}, {dens})".format(
+        xi=xi,
+        xo=xo,
+        r=r,
+        ri=ri,
+        den_cone=den_cone,
+        dens=initial_density
+    )
+    initial_density = "where((x > {xo}), 0.0, {dens}".format(
+        xo=xo,
+        dens=initial_density
+    )
+    plasma_distribution = picmi.AnalyticDistribution(
+        density_expression=initial_density,
+        th=th
+    )
+
+    plasma = picmi.MultiSpecies(
+        particle_types=["proton", "electron"],
+        names=["proton", "electron"],
+        initial_distribution=plasma_distribution
+    )
+
+    plasma_layout = picmi.PseudoRandomLayout(
+        n_macroparticles=4 * 250 * 250
+    )
+
+    laser = picmi.GaussianLaser(
+        wavelength=lamb,
+        waist=2 / math.sqrt(2) * 2.5 * micron,
+        duration=max_time,
+        E0=1e13
+    )
+    antena = picmi.LaserAntenna(
+        position=[xmin, (ymax + ymin) / 2],
+        normal_vector=[0, 1]
+    )
+
+    normal_diag_particles = picmi.ParticleDiagnostic(
+        epoch_dt_snapshot=1 * femto,
+        data_list=["position", "momentum", "weighting"],
+        name="normal"
+    )
+
+    normal_diag_field = picmi.FieldDiagnostic(
+        grid=grid,
+        epoch_dt_snapshot=1 * femto,
+        data_list=["E"],
+        name="normal"
+    )
+
+    simulation.add_diagnostic(normal_diag_particles)
+    simulation.add_diagnostic(normal_diag_field)
+
+    simulation.add_laser(laser, injection_method=antena)
+
+    simulation.add_species(plasma, plasma_layout)
+    return simulation
 
 
 def main():
-    epoch_exe = "/home/martin/CLionProjects/epoch-4.17.14/epoch2d/bin/epoch2d"
-    with subprocess.Popen(
-            ["mpirun", "-np", "4", epoch_exe],
-            stdin=subprocess.PIPE
-    ) as proc:
-        proc.communicate(b"cone")
+    simulation = get_simulation()
+    simulation.set_epoch_path("/home/martin/Sources/epoch-4.17.16/epoch2d/bin/epoch2d")
+    simulation.set_processors_count(4)
+    simulation.step()
 
 
 if __name__ == '__main__':
--- pyepoch/picmi/simulation.py
+++ pyepoch/picmi/simulation.py
@@ -1,31 +1,60 @@
+import os
+import subprocess
+
 import picmistandard
+
+from pyepoch.picmi.exceptions import PyEpochError
 from pyepoch.picmi.grids import Cartesian2DGrid
 from pyepoch.picmi.parsers import write_control, write_boundaries, parse_layouts_species, infer_charge_mass, \
     write_species, parse_diagnostics, write_diagnostic, write_laser, parse_laser_method
 
 
 class Simulation(picmistandard.PICMI_Simulation):
+    def __init__(self, solver, **kw):
+        self.epoch_exe = ""
+        self.processors_count = 1
+        super().__init__(solver, **kw)
+
     def step(self, nsteps=1):
-        pass
+        if not self.epoch_exe:
+            raise PyEpochError("epoch path not specified")
+
+        os.mkdir("output")
+        self.write_input_file("output/input.deck")
+
+        with subprocess.Popen(
+                ["mpirun", "-np", "{}".format(self.processors_count), self.epoch_exe],
+                stdin=subprocess.PIPE
+        ) as proc:
+            proc.communicate(b"output")
 
     def init(self, kw):
         pass
 
     def write_input_file(self, file_name):
-        grid = self.solver.grid
-        if not isinstance(grid, Cartesian2DGrid):
-            raise Exception('pyepoch supports only Cartesian2DGrid')
         with open(file_name, "w") as f:
-            write_control(grid, self.max_time, f)
-            write_boundaries(grid, f)
-            parsed_species = parse_layouts_species(self.layouts, self.species)
-            parsed_species = [infer_charge_mass(ps) for ps in parsed_species]
-            for s in parsed_species:
-                write_species(s, f)
-            parsed_diagnostics = parse_diagnostics(self.diagnostics)
-            for d in parsed_diagnostics:
-                write_diagnostic(d, 2, f)  # TODO remove hardcoded dimension
+            self.write_to_opened_file(f)
 
-            parsed_lasers = [parse_laser_method(r, m, grid) for r, m in zip(self.lasers, self.laser_injection_methods)]
-            for r in parsed_lasers:
-                write_laser(r, f)
+    def write_to_opened_file(self, f):
+        grid = self.solver.grid
+        if not isinstance(self.solver.grid, Cartesian2DGrid):
+            raise Exception('pyepoch supports only Cartesian2DGrid')
+        write_control(grid, self.max_time, f)
+        write_boundaries(grid, f)
+        parsed_species = parse_layouts_species(self.layouts, self.species)
+        parsed_species = [infer_charge_mass(ps) for ps in parsed_species]
+        for s in parsed_species:
+            write_species(s, f)
+        parsed_diagnostics = parse_diagnostics(self.diagnostics)
+        for d in parsed_diagnostics:
+            write_diagnostic(d, 2, f)  # TODO remove hardcoded dimension
+
+        parsed_lasers = [parse_laser_method(r, m, grid) for r, m in zip(self.lasers, self.laser_injection_methods)]
+        for r in parsed_lasers:
+            write_laser(r, f)
+
+    def set_processors_count(self, count):
+        self.processors_count = count
+
+    def set_epoch_path(self, epoch_path):
+        self.epoch_exe = epoch_path
